b2582184734106418fbffddee7dc6c4bd18f2330
Ole-ks <rusovaolya@gmail.com>
2017-09-15 18:01:30 +0300

    first

---
+++ site.js
@@ -0,0 +1,50 @@
+// Перечисление зависимостей:
+var path = require('path');
+var express = require('express');
+var fs = require('fs');
+
+function getFiles(dir){
+  fileList = [];
+  var files = fs.readdirSync(dir);
+  for(var i in files){
+    if (!files.hasOwnProperty(i)) continue;
+    var name = dir+files[i];
+    if (!fs.statSync(name).isDirectory()){
+      fileList.push(name);
+    }
+  }
+  return fileList;
+}
+var arr = getFiles('./static/sounds/long_breaks/');
+var file = fs.createWriteStream('./static/long_breaks.txt');
+file.on('error', function(err) { console.log('err');});
+arr.forEach(function(v) { file.write('/sounds/long_breaks/' + v.split('\\').pop().split('/').pop() + ';;'); });
+file.end();
+
+var arr = getFiles('./static/sounds/short_breaks/');
+var file = fs.createWriteStream('./static/short_breaks.txt');
+file.on('error', function(err) { console.log('err');});
+arr.forEach(function(v) { file.write('/sounds/short_breaks/' + v.split('\\').pop().split('/').pop() + ';;'); });
+file.end();
+
+var arr = getFiles('./static/sounds/answers/');
+var file = fs.createWriteStream('./static/answers.txt');
+file.on('error', function(err) { console.log('err');});
+arr.forEach(function(v) { file.write('/sounds/answers/' + v.split('\\').pop().split('/').pop() + ';;'); });
+file.end();
+
+// Описание настроек:
+var staticSiteOptions = {
+   portnum: 80, // слушать порт 80
+   maxAge: 1000 * 60 * 1 // хранить страницы в кэше пятнадцать минут
+};
+
+// Запуск сайта:
+express().use(express.static(
+   path.join(__dirname, 'static'),
+   staticSiteOptions
+)).listen(staticSiteOptions.portnum);
+
+console.log("it works!");
+console.log("http://127.0.0.1/");
+
